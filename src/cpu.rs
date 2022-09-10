526bd3d4525b8d8f4427dfb1cd77165e69479b24
Óskar Jörgen Sandholt <oskarjorgen@gmail.com>
2022-09-10 16:24:26 +0000

    Y was not reset properly

--- src/cpu.rs
+++ src/cpu.rs
@@ -135,6 +135,7 @@ impl CPU {
     pub fn reset(&mut self) {
         self.register_a = 0;
         self.register_x = 0;
+        self.register_y = 0;
         self.status = 0;
 
         self.program_counter = self.mem_read_u16(RESET_START);
